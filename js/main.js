81cb7e0c3c85a16a276b772eb1372ccc0ec10f0f
Shasper0 <109701515+Shasper0@users.noreply.github.com>
2022-10-04 19:43:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// let number = document.querySelectorAll('.stats .number')
+// let section = document.querySelector(".stats");
+
+
+
+// window.onscroll = function(){
+//     if(window.scrollY >= section.offsetTop){
+//         number.forEach((number)=> startCount(number))
+//     }
+// }
+
+// function startCount(el){
+//     let goal = el.dataset.goal;
+//     let count = setInterval(()=>{
+//         el.textContent++;
+//         if(el.textContent == goal ){
+//             clearInterval(count)
+//         }
+//     }, 10)
+// }
+// startCount(document.querySelectorAll('.stats .number')[0])
